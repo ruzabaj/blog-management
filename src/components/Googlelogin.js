ac8ae590d117ac95247040be8974dc6540c34b92
userbaj <64317950+userbaj@users.noreply.github.com>
2022-06-27 19:26:30 +0545

    google sign in added

---
+++ src/components/Googlelogin.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+
+const clientId = "658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com";
+
+function Login() {
+
+    const [showloginButton, setShowloginButton] = useState(true);
+    const [showlogoutButton, setShowlogoutButton] = useState(false);
+
+    const onLoginSuccess = (res) => {
+        console.log('Login Success:', res.profileObj);
+        setShowloginButton(false);
+        setShowlogoutButton(true);
+    };
+
+    const onLoginFailure = (res) => {
+        console.log('Login Failed:', res);
+    };
+
+    const onSignoutSuccess = () => {
+        alert("You have been logged out successfully");
+        console.clear();
+        setShowloginButton(true);
+        setShowlogoutButton(false);
+    };
+
+    return (
+        <div>
+            { showloginButton ?
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign In with Google"
+                    onSuccess={onLoginSuccess}
+                    onFailure={onLoginFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                /> : null}
+
+            { showlogoutButton ?
+                <GoogleLogout
+                    clientId={clientId}
+                    buttonText="Sign Out"
+                    onLogoutSuccess={onSignoutSuccess}
+                >
+                </GoogleLogout> : null
+            }
+        </div>
+    );
+}
+export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,50 +1,98 @@
 import React from "react";
 import "../sass/signup.scss";
+import GoogleSignUp from './Googlelogin'
+import { useForm } from "react-hook-form";
 
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  //   const { onChange, onBlur, name, ref } = register("firstName");
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="signup-container">
       <div className="bg-img">
         <img src="background.jpg" alt="background" />
-        <div className="quote">
-            Explore your creativity!
-        </div>
+        <div className="quote">Explore your creativity!</div>
       </div>
       <div className="signup-right-container">
         <h1>Create an Account</h1>
         <p>Let's get started with your 30 day free trial.</p>
+
         <div className="signup-form">
           <div className="form">
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="form-group">
                 <div className="form-username">
                   <label>Username</label>
                 </div>
-                <input type="text" name="Username" />
+                {/* onChange={onChange}  name={name} onBlur={onBlur}  ref={ref} */}
+                <input
+                  type="text"
+                  {...register("Username", {
+                    required: true,
+                    maxLength: 20,
+                    minLength: 5,
+                    onChange: (e) => console.log(e),
+                  })}
+                />
+                {errors.Username && (
+                  <div className="error-message">This field is required</div>
+                )}
               </div>
               <div className="form-group">
                 <div className="form-email">
                   <label>Email</label>
                 </div>
-                <input type="text" name="email" />
+                <input
+                  type="text"
+                  name="email"
+                  {...register("Email", {
+                    required: true,
+                    pattern: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$/,
+                  })}
+                />
+                {errors.Email && (
+                  <div className="error-message">Use correct email format</div>
+                )}
               </div>
 
               <div className="form-group">
                 <div className="form-password">
                   <label>Password</label>
                 </div>
-                <input type="password" name="password" />
+                <input
+                  type="password"
+                  name="password"
+                  {...register("password", {
+                    required: true,
+                    pattern:
+                      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+                  })}
+                />
+                {errors.password && (
+                  <div className="error-message">
+                    Pawword should have at least 8 character
+                  </div>
+                )}
               </div>
               <div className="btn">
                 <div className="btn-create-acc">
+                  <input type="submit" />
                   <button type="submit" className="create-account">
                     Create an Account
                   </button>
                 </div>
                 <div className="btn-signup-google">
-                  <button type="submit" className="create-account">
-                    Sign Up with Google
-                  </button>
+                  {/* <button className="create-account">
+                  </button> */}
+                  <GoogleSignUp/>
                 </div>
               </div>
               <div className="link-login">
