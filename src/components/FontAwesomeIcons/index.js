338c0186ca4d03e69551dffe0faa206219de5d60
userbaj <64317950+userbaj@users.noreply.github.com>
2022-06-28 22:49:11 +0545

    fontawesome icon:css changes

--- src/components/FontAwesomeIcons/index.js
+++ src/components/FontAwesomeIcons/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import '../../sass/icon.scss';
 
 const index = () => {
   return (
