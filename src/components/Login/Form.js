97863a18e55ac52de858f465f318698d5e1ab1d7
userbaj <64317950+userbaj@users.noreply.github.com>
2022-06-28 16:59:16 +0545

    checkox using usestate

--- src/components/Login/Form.js
+++ src/components/Login/Form.js
@@ -1,7 +1,7 @@
 import React from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import Router from "../../Router";
 
 const Form = () => {
   const {
@@ -9,6 +9,9 @@ const Form = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+
+  const [checked, setChecked] = useState(true);
+
   const onSubmit = (data) => {
     // alert(JSON.stringify(data)),
     console.log(data);
@@ -17,73 +20,72 @@ const Form = () => {
   return (
     <div className="login-container">
       <div className="signup-right-container">
-          <h2>Hello, Again!</h2>
-          <p>
-            Welcome to Creativity, please put your login credentials below to
-            start using the app.
-          </p>
+        <h2>Hello, Again!</h2>
+        <p>
+          Welcome to Creativity, please put your login credentials below to
+          start using the app.
+        </p>
 
-          <div className="signup-form">
-            <div className="form">
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-group">
-                  <div className="form-email">
-                    <label>Email</label>
-                  </div>
-                  <input
-                    type="text"
-                    name="email"
-                    {...register("Email", {
-                      required: true,
-                      pattern: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$/,
-                    })}
-                  />
-                  {errors.Email && (
-                    <div className="error-message">
-                      Use correct email format
-                    </div>
-                  )}
+        <div className="signup-form">
+          <div className="form">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-group">
+                <div className="form-email">
+                  <label>Email</label>
                 </div>
+                <input
+                  type="text"
+                  name="email"
+                  {...register("Email", {
+                    required: true,
+                    pattern: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$/,
+                  })}
+                />
+                {errors.Email && (
+                  <div className="error-message">Use correct email format</div>
+                )}
+              </div>
 
-                <div className="form-group">
-                  <div className="form-password">
-                    <label>Password</label>
-                    <a href="forgot-pw">Forgot password?</a>
+              <div className="form-group">
+                <div className="form-password">
+                  <label>Password</label>
+                  <a href="forgot-pw">Forgot password?</a>
+                </div>
+                <input
+                  type="password"
+                  name="password"
+                  {...register("password", {
+                    required: true,
+                    pattern: /^[a-zA-Z0-9+_.-]/,
+                  })}
+                />
+                {errors.password && (
+                  <div className="error-message">
+                    Pawword should have at least 8 character
                   </div>
-                  <input
-                    type="password"
-                    name="password"
-                    {...register("password", {
-                      required: true,
-                      pattern: /^[a-zA-Z0-9+_.-]/,
-                    })}
-                  />
-                  {errors.password && (
-                    <div className="error-message">
-                      Pawword should have at least 8 character
-                    </div>
-                  )}
+                )}
+              </div>
+              <div className="btn">
+                <input type="checkbox" onChange={() => setChecked(!checked)} />
+                {console.log(checked,"value of check using usestate hook")}
+                <label>Remember me</label>
+                <div className="btn-create-acc">
+                  <button type="submit" className="login-button">
+                    Login
+                  </button>
                 </div>
-                <div className="btn">
-                  <input type="checkbox" />
-                  <label>Remember me</label>
-                  <div className="btn-create-acc">
-                    <button type="submit" className="login-button">
-                      Login
-                    </button>
-                  </div>
-                </div>
-                <div className="link-login">
-                  <p>Not a member?</p>
-                  <span>
-                    <Link to="/signup">Sign Up</Link>
-                  </span>
-                </div>
-              </form>
-            </div>
+              </div>
+              <div className="link-login">
+                <p>Not a member?</p>
+                <span>
+                  <Link to="/signup">Sign Up</Link>
+                </span>
+              </div>
+            </form>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
