9d4ba1469128c8ce951313bfea4069db156884da
userbaj <64317950+userbaj@users.noreply.github.com>
2022-09-27 18:30:03 +0545

    added:card in dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import Sidebar from './sidebar';
-// import {Redirect} from 'react-router-dom';
+import React from "react";
+import Sidebar from "./Navbar";
 
-const Dashboard = ({authorized}) => {
+const Dashboard = () => {
   return (
     <div>
-      if(!authorized){
-        // <Redirect to="/login"/>
-      }
-        <Sidebar/>
-        <h2>You have entered in the dashboard</h2>
+      <Sidebar />
+      <h2>You have entered in the dashboard</h2>
+      <div class="card">
+        <img src="user-profile.jpg" class="card-img-top" alt="user-profile" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="user " class="btn btn-primary">
+            User Profile
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
