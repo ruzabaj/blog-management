cac1853417efc0a5ffc9a3a033235e251c37aa70
userbaj <64317950+userbaj@users.noreply.github.com>
2022-09-27 19:25:59 +0545

    added:dataset

---
+++ src/Data.js
@@ -0,0 +1,50 @@
+export const Userdata=[
+    {
+        id:10001,
+        image:'profile-one.jpg',
+        name:'user-name',
+        description:'user-detail',
+        arrivedTime:10,
+        salary:'10k'
+    },
+    {
+        id:10001,
+        image:'profile-one.jpg',
+        name:'user-name',
+        description:'user-detail',
+        arrivedTime:10,
+        salary:'10k'
+    },
+    {
+        id:10001,
+        image:'profile-one.jpg',
+        name:'user-name',
+        description:'user-detail',
+        arrivedTime:10,
+        salary:'10k'
+    },
+    {
+        id:10003,
+        image:'profile-two.jpg',
+        name:'user-name',
+        description:'user-detail',
+        arrivedTime: 11,
+        salary:'10k'
+    },
+    {
+        id:10004,
+        image:'profile-four.jpg',
+        name:'user-name',
+        description:'user-detail',
+        arrivedTime:10,
+        salary:'30k'
+    },
+    {
+        id:10002,
+        image:'profile-two.jpg',
+        name:'user-name',
+        description:'user-detail',
+        arrivedTime: 9,
+        salary:'20k'
+    },
+]
