3036f6b63207c64356bd5821b3bca27fd90aee62
userbaj <64317950+userbaj@users.noreply.github.com>
2022-10-14 20:07:21 +0545

    added:css for togglr

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./App.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
