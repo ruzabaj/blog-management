e0b83519ac472dbcf97e3468c40496f9f134945f
userbaj <64317950+userbaj@users.noreply.github.com>
2022-10-14 16:28:37 +0545

    added: toggle component

---
+++ src/components/Dashboard/Create/create.js
@@ -0,0 +1,82 @@
+import React, { useState , useRef} from 'react';
+import Dropdown from "react-dropdown";
+import "../../../sass/create.scss";
+
+const Create = () => {
+    //for dropdown
+    const options = [
+        { value: "Time", label: "Time" },
+        { value: "Two", label: "Music" },
+        { value: "Three", label: "Comedy" },
+        { value: "Four", label: "Life" },
+        { value: "Five", label: "Style" },
+      ];
+      const [value, setValue] = useState("");
+    
+      const handleChange = (e) => {
+        setValue(e.target.value);
+      };
+
+      //for  uploading file
+      const  [file, setFile]= useState(false);
+    
+      const fileUpload = (event) => {
+        setFile(event.target.files[0].name);
+      };
+
+      const fileInputField = useRef(null)
+
+  return (
+    <div>
+        <div className="create-bg">
+        <img src=" write-bg.jpg" alt="bg" className="background-create" />
+      </div>
+      <div className="create-width">
+        <div className="create-center-align">
+          <div className="manuals">
+            <div className="create-blog-category">
+                <p>{file}</p>
+                <input type="file" 
+                onChange={fileUpload}
+                ref={fileInputField}
+                style={{ display: "none" }} 
+                />
+              <button>
+                <img src="add.png" alt="add" className="create-blog" />
+              </button>
+
+              <Dropdown
+                className="category"
+                id="category"
+                options={options}
+                onChange={handleChange}
+                value={value}
+                placeholder="Select a category"
+              />
+            </div>
+            <button className="post">POST</button>
+          </div>
+
+          <div className="text-area">
+            <textarea
+              rows="1"
+              cols="0"
+              minLength="05"
+              maxLength="20"
+              placeholder="Title"
+            />
+            <textarea
+              rows="10"
+              cols="1"
+              minLength="100"
+              maxLength="2000"
+              placeholder="Write something here…"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Create
--- src/components/Dashboard/Create/index.js
+++ src/components/Dashboard/Create/index.js
@@ -1,81 +1,16 @@
-import React, { useState } from "react";
-import "../../../sass/create.scss";
-import Dropdown from "react-dropdown";
+import React from "react";
 import Navbar from "../Navbar";
 import Footer from "../footer";
+import Create from "./create"
 
-const Create = () => {
-  const options = [
-    { value: "Time", label: "Time" },
-    { value: "Two", label: "Music" },
-    { value: "Three", label: "Comedy" },
-    { value: "Four", label: "Life" },
-    { value: "Five", label: "Style" },
-  ];
-  const [value, setValue] = useState("");
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  const uploadImage = (event) => {
-    console.log(event.target.value);
-  };
-
+const Index = () => {
   return (
     <div>
       <Navbar />
-      <div className="create-bg">
-        <img src=" write-bg.jpg" alt="bg" className="background-create" />
-      </div>
-      <div className="create-width">
-        <div className="create-center-align">
-          <div className="manuals">
-            <div className="create-blog-category">
-        {/* <button onClick={uploadImage}>
-               <img src="add.png" alt="add" className="create-blog" />
-          <input type="file" />
-        </button> */}
-      <button>
-      <img src="add.png" alt="add" className="create-blog" />
-        <input type="file"
-         style={{display:"none"}}
-         />
-      </button>
-              
-              <Dropdown
-                className="category"
-                id="category"
-                options={options}
-                onChange={handleChange}
-                value={value}
-                placeholder="Select a category"
-              />
-            </div>
-            <button className="post">POST</button>
-          </div>
-
-          <div className="text-area">
-            <textarea
-              rows="1"
-              cols="0"
-              minLength="05"
-              maxLength="20"
-              placeholder="Title"
-            />
-            <textarea
-              rows="10"
-              cols="1"
-              minLength="100"
-              maxLength="2000"
-              placeholder="Write something here…"
-            />
-          </div>
-        </div>
-      </div>
-      <Footer/>
+      <Create/>
+      <Footer />
     </div>
   );
 };
 
-export default Create;
+export default Index;
