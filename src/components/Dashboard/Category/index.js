16e66f1a3f8568d910d733143ebd7c81c16118a2
userbaj <64317950+userbaj@users.noreply.github.com>
2022-10-23 23:49:36 +0545

    delete: category

--- src/components/Dashboard/Category/index.js
+++ src/components/Dashboard/Category/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../../../sass/blog.scss";
 import "../../../sass/category.scss";
@@ -8,6 +8,8 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "../Navbar";
 
 const Category = () => {
+  const [categoryTitle, setCategoryTitle] = useState([]);
+  const [activeCategory, setActiveCategory] = useState(false);
   const [categoryDetail, setCategoryDetail] = useState({
     categoryname: "",
     categorystatus: "",
@@ -32,12 +34,46 @@ const Category = () => {
         status: categoryDetail.categorystatus,
       })
       .then((res) => {
-        console.log(res,"res from then");
+        console.log(res, "res from post category detail");
       })
       .catch((err) => {
         console.log(err);
       });
   };
+
+  useEffect(() => {
+    axios
+      .get("http://api.allureinternational.com.np/api/get-all-category")
+      .then((response) => {
+        console.log(
+          "inside array for accessing the title",
+          response.data.data[0].category_name
+        );
+        setCategoryTitle(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const showactiveCategory = () => {
+    // categoryTitle.filter
+    if (categoryTitle.status === "active") {
+      setActiveCategory(true);
+    } else {
+      setActiveCategory(false);
+    }
+  };
+  const deleteCategory=(id)=>{
+    axios.delete(`http://api.allureinternational.com.np/api/delete-category/${id}`)
+    .then((res)=>{
+      console.log(res)
+      setCategoryDetail(res.data.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
   return (
     <div>
       <Navbar />
@@ -60,18 +96,46 @@ const Category = () => {
           </div>
           <div className="category-status">
             <label>Status:</label>
-            <select name="status"  id="categorystatus" onChange={handleChange}>
+            <select name="status" id="categorystatus" onChange={handleChange}>
               <option value="active">Active</option>
-              <option value="inactive" >Inactive</option>
+              <option value="inactive">Inactive</option>
             </select>
           </div>
         </div>
-          <div className="btn-submit">
-            <button type="submit" className="category-btn" onSubmit={handleSubmit}             >
-              Submit
-            </button>
-          </div>
+        <div className="btn-submit">
+          <button
+            type="submit"
+            className="category-btn"
+            onSubmit={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
       </form>
+      {/* activeCategory&& */}
+      <div class="table-responsive-lg">
+        <h5>List of Categories</h5>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Category</th>
+              <th scope="col">Delete Option</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categoryTitle.map((element, index) => (
+              <tr key={index}>
+                <th scope="row">{element.category_name}</th>
+                <th>
+                  <button className="delete-category-btn" onClick={()=>deleteCategory(element.id)}>
+                    Delete Category
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
