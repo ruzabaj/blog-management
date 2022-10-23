99e827ef8889837047b26291a3ec85fd49f0d868
userbaj <64317950+userbaj@users.noreply.github.com>
2022-10-23 17:04:16 +0545

    added:back to home btn in category

--- src/components/Dashboard/Category/index.js
+++ src/components/Dashboard/Category/index.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import "../../../sass/blog.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faArrowAltCircleLeft} from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from 'react-router-dom';
 
 const Category = () => {
+    let navigate= useNavigate();
+    const BacktoHme=()=>{
+        navigate('/home');
+    }
   return (
-    <div>Category</div>
+    <div>
+        <h1>Category</h1>
+        <FontAwesomeIcon icon={faArrowAltCircleLeft} onClick={BacktoHme} className="back"/>
+    </div>
   )
 }
 
