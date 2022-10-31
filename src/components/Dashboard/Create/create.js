935c3811e4e70147820493266dcebf5d85605d35
userbaj <64317950+userbaj@users.noreply.github.com>
2022-10-31 20:25:44 +0545

    add

--- src/components/Dashboard/Create/create.js
+++ src/components/Dashboard/Create/create.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import Dropdown from "react-dropdown";
+import React, { useState, useEffect } from "react";
 import JoditEditor from "jodit-react";
 import "../../../sass/create.scss";
 import axios from "axios";
@@ -19,10 +18,6 @@ const Create = () => {
     { value: "Six", label: "Travel" },
   ];
 
-  const handleChange = (e) => {
-    setCategory(e.target.value);
-  };
-
   const imageUpload = (event) => {
     console.log(event.target.files);
     setImage(event.target.files[0]);
@@ -38,7 +33,14 @@ const Create = () => {
       });
   };
 
-
+  useEffect(() => {
+    axios
+      .get("http://api.allureinternational.com.np/api/get-all-active-category")
+      .then((res) => {
+        console.log(res);
+        setCategory(res.data.data);
+      });
+  }, []);
   return (
     <div>
       <div className="create-bg">
@@ -48,6 +50,7 @@ const Create = () => {
         <div className="create-center-align">
           <form>
             <div className="text-area">
+              <label>Title:</label>
               <textarea
                 rows="1"
                 cols="0"
@@ -56,44 +59,49 @@ const Create = () => {
                 placeholder="Title"
                 className="title"
               />
-              {/* <textarea
-              rows="05"
-              cols="0"
-              minLength="100"
-              maxLength="2000"
-              placeholder="Write something here…"
-              className='description'
-            /> */}
+              <div className="create-blog-category">
+                <select
+                  name="category-list"
+                  id="category-list"
+                  // value={}
+                  // onChange={handleChange}
+                >
+                  <option value="#"></option>
+                  <option value="travel">Travel</option>
+                </select>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={imageUpload}
+                  className="postimage"
+                  name="file"
+                />
+                <label for="file">Upload</label>
+              </div>
+              <label>Description:</label>
               <JoditEditor
                 value={content}
                 tabIndex={1} // tabIndex of textarea
                 onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
                 onChange={(newContent) => {}}
               />
+              <textarea
+                rows="10"
+                cols="100"
+                minLength="05"
+                maxLength="1000"
+                placeholder="Description"
+                className="description"
+              />
+              <p style={{ color: "grey", size: "10px" }}>
+                Do not exceed more than 1000 characters.
+              </p>
             </div>
           </form>
-          <div className="manuals">
-            <div className="create-blog-category">
-              <input
-                type="file"
-                onChange={imageUpload}
-                className="postImage"
-                name="file"
-              />
-              <select
-                name="category-list"
-                id="category-list"
-                // value={}
-                onChange={handleChange}
-              >
-                <option value="#"></option>
-                <option value="travel">Travel</option>
-              </select>
-            </div>
-            <button type="submit" className="post" onClick={handlePost}>
-              POST
-            </button>
-          </div>
+
+          <button type="submit" className="post" onClick={handlePost}>
+            POST
+          </button>
         </div>
       </div>
     </div>
