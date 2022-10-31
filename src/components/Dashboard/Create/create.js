98e637b098539b0a2c165470882cfe92d6a94f9a
userbaj <64317950+userbaj@users.noreply.github.com>
2022-10-31 22:11:35 +0545

    completed:blog post with image uploaded preview

--- src/components/Dashboard/Create/create.js
+++ src/components/Dashboard/Create/create.js
@@ -4,43 +4,59 @@ import "../../../sass/create.scss";
 import axios from "axios";
 
 const Create = () => {
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState({
+    title: "",
+    category: "",
+    description: "",
+    status: "",
+  });
   const [category, setCategory] = useState([]);
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState();
 
   useEffect(() => {
     axios
-    .get("http://api.allureinternational.com.np/api/get-all-active-category")
-    .then((response) => {
+      .get("http://api.allureinternational.com.np/api/get-all-active-category")
+      .then((response) => {
         console.log(response.data.data);
-        setCategory(response.data.data)
+        setCategory(response.data.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  },[]);
- 
+  }, []);
+
   const imageUpload = (event) => {
     console.log(event.target.files);
-    setImage(event.target.files[0]);
+    // setImage(event.target.files[0]);
+    setImage(URL.createObjectURL(event.target.files[0]))
   };
 
   const handlePost = () => {
     const formData = new FormData();
     formData.append("image", image);
+    formData.append("title", content.title);
+    formData.append("category_id", content.category);
+    formData.append("description", content.description);
+    formData.append("status", content.status);
     axios
       .post("http://api.allureinternational.com.np/api/add-blog", formData)
       .then((res) => {
         console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
       });
   };
-
+  const handleChange = (event) => {
+    const newdata = { ...content };
+    newdata[event.target.id] = event.target.value;
+    setContent(newdata);
+    console.log(newdata, "newdata");
+  };
 
   return (
     <div>
-      <div className="create-bg">
-        {/* <img src=" write-bg.jpg" alt="bg" className="background-create" /> */}
-      </div>
+      <div className="create-bg"></div>
       <div className="create-width">
         <div className="create-center-align">
           <form>
@@ -53,17 +69,32 @@ const Create = () => {
                 maxLength="20"
                 placeholder="Title"
                 className="title"
+                id="title"
+                value={content.title}
+                onChange={handleChange}
               />
               <div className="create-blog-category">
                 <select
                   name="category-list"
-                  id="category-list"
-                  // value={}
-                  // onChange={handleChange}
+                  id="category"
+                  value={content.category}
+                  onChange={handleChange}
                 >
                   {category.map((element, index) => (
-                    <option value={element.id} key={index}>{element.category_name}</option>
-                  ))} 
+                    <option value={element.id} key={index}>
+                      {element.category_name}
+                    </option>
+                  ))}
+                </select>
+                <select
+                  name="active-status"
+                  id="status"
+                  value={content.status}
+                  onChange={handleChange}
+                >
+                  <option value="#"></option>
+                  <option value="active">active</option>
+                  <option value="inactive">inactive</option>
                 </select>
                 <input
                   type="file"
@@ -82,16 +113,21 @@ const Create = () => {
                 maxLength="1000"
                 placeholder="Description"
                 className="description"
+                id="description"
+                onChange={handleChange}
+                value={content.description}
               />
               <p style={{ color: "grey", size: "10px" }}>
                 Do not exceed more than 1000 characters.
               </p>
             </div>
           </form>
-
           <button type="submit" className="post" onClick={handlePost}>
             POST
           </button>
+          <div className="upload-image-class">
+          <img src={image} alt="pic" classname="uploaded-image"/>
+          </div>
         </div>
       </div>
     </div>
