473e324b363029a3779958436994c77c5e0b34d3
userbaj <64317950+userbaj@users.noreply.github.com>
2022-11-04 15:25:54 +0545

    change in footer

--- src/components/Dashboard/footer.js
+++ src/components/Dashboard/footer.js
@@ -5,34 +5,38 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-content">
-        <div className="social-media">
-          <h4 className="follow">Follow Us</h4>
-          <div className="social-media-icon">
-            <img src="insta.jpg" alt="insta" className="insta" />
-            <img src="facebook.png" alt="facebook" className="facebook" />
-            <img src="twitter.png" alt="twitter" className="twitter" />
-          </div>
-        </div>
+        <img src="logo.jpg" alt="logo" className="logo" />
         <div className="about">
           <h5 className="about">About</h5>
           <ul>
-            <li>Company</li>
             <li>Career</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Use</li>
           </ul>
         </div>
         <div className="blogs-link">
           <h5>Blog</h5>
           <ul>
-            <li>Travel</li>
-            <li>Travel</li>
-            <li>Travel</li>
-            <li>Travel</li>
-            <li>Travel</li>
+            <li>Latest Post</li>
+            <li>Popular Post</li>
+            <li>Buy Theme</li>
           </ul>
         </div>
       </div>
+
       <div className="horizontal" />
+
+      <div className="social-media">
+        <div className="social-media-icon">
+          <img src="insta.jpg" alt="insta" className="insta" />
+          <img src="facebook.png" alt="facebook" className="facebook" />
+          <img src="twitter.png" alt="twitter" className="twitter" />
+        </div>
+      </div>
       <p className="copyright">© Copyright 2022</p>
+      <p className="copyright-message">
+        With a commitment to quality content for our community.
+      </p>
     </div>
   );
 };
